41530781131632069540b8857a208d32024cdac3
ryan <sally41176789@yahoo.com.tw>
2022-07-21 15:31:25 +0800

    fix(criteria): fix import

--- pages/criteria.js
+++ pages/criteria.js
@@ -10,6 +10,7 @@ import {
 import {
   merkleTree,
   buildResolver,
+  toBN,
 } from "../utils"
 
 export default function Criteria() {
@@ -32,39 +33,39 @@ export default function Criteria() {
 
   const { root, proofs } = merkleTree(ERC1155TokenIds);
 
-  const offer = [
-    // ERC721
-    // getOfferOrConsiderationItem(4, ERC721Token, root, toBN(1), toBN(1))
+  // const offer = [
+  //   // ERC721
+  //   // getOfferOrConsiderationItem(4, ERC721Token, root, toBN(1), toBN(1))
 
-    // ERC1155
-    // getOfferOrConsiderationItem(5, ERC1155Token, root, toBN(10), toBN(10))
+  //   // ERC1155
+  //   // getOfferOrConsiderationItem(5, ERC1155Token, root, toBN(10), toBN(10))
 
-    // collection-level
-    // getOfferOrConsiderationItem(4, ERC721Token, constants.HashZero, toBN(1), toBN(1))
+  //   // collection-level
+  //   // getOfferOrConsiderationItem(4, ERC721Token, constants.HashZero, toBN(1), toBN(1))
 
-    // ERC20
-    getOfferOrConsiderationItem(1, wrapToken, 0, toBN("110000000000000000"), toBN("110000000000000000"))
-  ];
+  //   // ERC20
+  //   getOfferOrConsiderationItem(1, wrapToken, 0, toBN("110000000000000000"), toBN("110000000000000000"))
+  // ];
 
-  const consideration = [
-    // * <=> ERC721
-    // getOfferOrConsiderationItem(2, ERC721Token, 57, 1, 1, account),
-    // getOfferOrConsiderationItem(2, ERC721Token, 58, 1, 1, account)
+  // const consideration = [
+  //   // * <=> ERC721
+  //   // getOfferOrConsiderationItem(2, ERC721Token, 57, 1, 1, account),
+  //   // getOfferOrConsiderationItem(2, ERC721Token, 58, 1, 1, account)
 
-    // * <=> NATIVE
-    // getItemETH(parseEther("0.1"), parseEther("0.1"), account),
-    // getItemETH(parseEther("0.01"), parseEther("0.01"), owner),
+  //   // * <=> NATIVE
+  //   // getItemETH(parseEther("0.1"), parseEther("0.1"), account),
+  //   // getItemETH(parseEther("0.01"), parseEther("0.01"), owner),
 
-    getOfferOrConsiderationItem(4, ERC721Token, constants.HashZero, toBN(1), toBN(1), account),
+  //   getOfferOrConsiderationItem(4, ERC721Token, constants.HashZero, toBN(1), toBN(1), account),
 
-    // * <=> ERC20
-    getOfferOrConsiderationItem(1, wrapToken, 0, toBN("10000000000000000"), toBN("10000000000000000"), zone),
-    getOfferOrConsiderationItem(1, wrapToken, 0, toBN("10000000000000000"), toBN("10000000000000000"), owner),
+  //   // * <=> ERC20
+  //   getOfferOrConsiderationItem(1, wrapToken, 0, toBN("10000000000000000"), toBN("10000000000000000"), zone),
+  //   getOfferOrConsiderationItem(1, wrapToken, 0, toBN("10000000000000000"), toBN("10000000000000000"), owner),
 
-    // * <=> ERC1155
-    // getOfferOrConsiderationItem(3, ERC1155Token, ERC1155TokenId3, 1, 1, account),
-    // getOfferOrConsiderationItem(3, ERC1155Token, ERC1155TokenId2, 1, 1, account),
-  ]
+  //   // * <=> ERC1155
+  //   // getOfferOrConsiderationItem(3, ERC1155Token, ERC1155TokenId3, 1, 1, account),
+  //   // getOfferOrConsiderationItem(3, ERC1155Token, ERC1155TokenId2, 1, 1, account),
+  // ]
 
   const criteriaResolvers = [
     // buildResolver(0, 0, 0, tokenId, proofs[tokenId.toString()]),
