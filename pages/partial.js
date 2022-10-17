4c581899aa63bb5ac5bce48b164bdb84f5f9c34e
ryan <sally41176789@yahoo.com.tw>
2022-10-17 17:56:37 +0800

    adjust partial

--- pages/partial.js
+++ pages/partial.js
@@ -2,63 +2,49 @@ import styles from "../styles/Home.module.css";
 import { useState, useEffect, useCallback } from "react";
 import { W3WalletDriver, useW3Wallet } from "../components/w3wallet";
 import { constants, Contract } from "ethers";
-import { approveABI, seaportABI, erc20ABI } from "../constants"
-import { 
-  owner, seaport, zone, ERC721Token, ERC1155Token, LOOT,
-} from "../constants/contracts"
-import {
-  parseEther,
-  toKey,
-  getOfferOrConsiderationItem,
-  getItemETH,
-  getItem20,
-  getItem721,
-  getItem1155,
-  getBasicOrderParameters,
-  createOrder,
-} from "../utils"
+import SDK from "../sdk"
 
 const OfferItem = ({ offer, setOffer, approve, cn }) => {
-  const { type } = offer
+  const { itemType } = offer
   return (
     <div className="space-x-2">
       {cn ? (
         <select
           id="offertype"
           value={offer.type}
-          onChange={(e) => setOffer({ ...offer, type: e.target.value})}
+          onChange={(e) => setOffer({ ...offer, itemType: Number(e.target.value)})}
           className="border-2 p-1 w-24"
         >
-          <option value="NATIVE">NATIVE</option>
-          <option value="20">20</option>
-          <option value="1155">1155</option>
+          <option value="0">NATIVE</option>
+          <option value="1">20</option>
+          <option value="3">1155</option>
         </select>
       ) : (
         <select
           id="offertype"
-          value={type}
-          onChange={(e) => setOffer({ ...offer, type: e.target.value})}
+          value={itemType}
+          onChange={(e) => setOffer({ ...offer, itemType: Number(e.target.value)})}
           className="border-2 p-1 w-24"
         >
-          <option value="1155">1155</option>
-          <option value="20">20</option>
+          <option value="1">20</option>
+          <option value="3">1155</option>
         </select>
       )}
-      {type !== "NATIVE" && <input
+      {itemType !== 0 && <input
         type="text"
         placeholder="address"
-        value={offer.address}
-        onChange={(e) => setOffer({ ...offer, address: e.target.value})}
+        value={offer.token}
+        onChange={(e) => setOffer({ ...offer, token: e.target.value})}
         className="border-2 p-1"
       />}
-      {type !== "721" && <input
+      {(itemType !== 2 && itemType !== 4) && <input
         type="number"
         placeholder="amount"
-        value={offer.amount || ""}
-        onChange={(e) => setOffer({ ...offer, amount: e.target.value})}
+        value={offer.startAmount || ""}
+        onChange={(e) => setOffer({ ...offer, startAmount: e.target.value})}
         className="border-2 p-1"
       />}
-      {(type === "721" || type === "1155") && <input
+      {itemType !== 0 && itemType !== 1 && <input
         type="number"
         placeholder="tokenId"
         value={offer.tokenId || ""}
@@ -72,7 +58,7 @@ const OfferItem = ({ offer, setOffer, approve, cn }) => {
         onChange={(e) => setOffer({ ...offer, recipient: e.target.value})}
         className="border-2 p-1"
       />}
-      {!offer.isApproved && type!== "NATIVE" && <button
+      {!offer.isApproved && itemType!== 0 && <button
         className="border px-2 p-1"
         onClick={() => approve(offer, setOffer)}
       >
@@ -81,202 +67,157 @@ const OfferItem = ({ offer, setOffer, approve, cn }) => {
     </div>
   )
 }
-
 export default function Home() {
   const [W3Wallet] = useW3Wallet()
   const { eProvider, signer, accounts, chainId } = W3Wallet
 
-  const marketplaceContract = new Contract(seaport, seaportABI, eProvider)
+  const sdk = eProvider && chainId && new SDK(eProvider, signer)
   const [order, setOrder] = useState(null)
   const [value, setValue] = useState(null)
   const [numerator, setNumerator] = useState(1)
   const [denominator, setDenominator] = useState(2)
   const [offer1, setOffer1] = useState({
     isApproved: false,
-    type: "1155",
-    address: "",
-    amount: undefined,
-    tokenId: undefined
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    itemType: 3
   })
   const [offer2, setOffer2] = useState({
     isApproved: false,
-    type: "1155",
-    address: "",
-    amount: undefined,
-    tokenId: undefined
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    itemType: 3
   })
   const [offer3, setOffer3] = useState({
     isApproved: false,
-    type: "1155",
-    address: "",
-    amount: undefined,
-    tokenId: undefined
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    itemType: 3
   })
   const [offer4, setOffer4] = useState({
     isApproved: false,
-    type: "1155",
-    address: "",
-    amount: undefined,
-    tokenId: undefined
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    itemType: 3
   })
   const [offer5, setOffer5] = useState({
     isApproved: false,
-    type: "1155",
-    address: "",
-    amount: undefined,
-    tokenId: undefined
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    itemType: 3
   })
   const [cn1, setCn1] = useState({
     isApproved: false,
-    type: "NATIVE",
-    address: "",
-    amount: undefined,
-    tokenId: undefined,
-    receipt: undefined
+    itemType: 0,
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    receipt: null,
+
   })
   const [cn2, setCn2] = useState({
     isApproved: false,
-    type: "NATIVE",
-    address: "",
-    amount: undefined,
-    tokenId: undefined,
-    receipt: undefined
+    itemType: 0,
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    receipt: null
   })
   const [cn3, setCn3] = useState({
     isApproved: false,
-    type: "NATIVE",
-    address: "",
-    amount: undefined,
-    tokenId: undefined,
-    receipt: undefined
+    itemType: 0,
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    receipt: null
   })
   const [cn4, setCn4] = useState({
     isApproved: false,
-    type: "NATIVE",
-    address: "",
-    amount: undefined,
-    tokenId: undefined,
-    receipt: undefined
+    itemType: 0,
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    receipt: null
   })
   const [cn5, setCn5] = useState({
     isApproved: false,
-    type: "NATIVE",
-    address: "",
-    amount: undefined,
-    tokenId: undefined,
-    receipt: undefined
+    itemType: 0,
+    token: "",
+    startAmount: null,
+    tokenId: null,
+    receipt: null
   })
 
   const account = accounts[0];
 
-  const checkIsApproved = useCallback(async (address, type, set) => {
-    let contract
-    let isApproved
-    if (type === "20") {
-      contract = new Contract(address, erc20ABI, eProvider)
-      const allowance = await contract.connect(signer).allowance(account, seaport)
-      if (allowance / Math.pow(10, 18) >= 10000000) {
-        isApproved = true
-      }
-    } else {
-      contract = new Contract(address, approveABI, eProvider)
-      isApproved = await contract.connect(signer).isApprovedForAll(account, seaport)
-    }
-    isApproved && set(prev => ({ ...prev, isApproved }))
-  }, [account, eProvider, signer])
+  const checkIsApproved = useCallback(async (offer, setState) => {
+    if ((offer.itemType === 2 || offer.itemType === 4) && !offer.tokenId) return
+    const isApproved = await sdk.loadApprovalStatus(offer, account)
+    isApproved && setState(prev => ({ ...prev, isApproved }))
+  }, [account, sdk])
 
   useEffect(() => {
-    offer1.address && checkIsApproved(offer1.address, offer1.type, setOffer1)
-    offer2.address && checkIsApproved(offer2.address, offer2.type, setOffer2)
-    offer3.address && checkIsApproved(offer3.address, offer3.type, setOffer3)
-    offer4.address && checkIsApproved(offer4.address, offer4.type, setOffer4)
-    offer5.address && checkIsApproved(offer5.address, offer5.type, setOffer5)
-  }, [checkIsApproved, offer1.address, offer1.type, offer2.address, offer2.type, offer3.address, offer3.type, offer4.address, offer4.type, offer5.address, offer5.type, offer1.isApproved, offer2.isApproved, offer3.isApproved, offer4.isApproved, offer5.isApproved])
+    offer1.token && checkIsApproved(offer1, setOffer1)
+    offer2.token && checkIsApproved(offer2, setOffer2)
+    offer3.token && checkIsApproved(offer3, setOffer3)
+    offer4.token && checkIsApproved(offer4, setOffer4)
+    offer5.token && checkIsApproved(offer5, setOffer5)
+  }, [checkIsApproved, offer1.token, offer1.tokenId, offer2.token, offer2.tokenId, offer3.token, offer3.tokenId, offer4.token, offer4.tokenId, offer5.token, offer5.tokenId])
 
   useEffect(() => {
-    cn1.address && checkIsApproved(cn1.address, cn1.type, setCn1)
-    cn2.address && checkIsApproved(cn2.address, cn2.type, setCn2)
-    cn3.address && checkIsApproved(cn3.address, cn3.type, setCn3)
-    cn4.address && checkIsApproved(cn4.address, cn4.type, setCn4)
-    cn5.address && checkIsApproved(cn5.address, cn5.type, setCn5)
-  }, [checkIsApproved, cn1.address, cn1.type, cn2.address, cn2.type, cn3.address, cn3.type, cn4.address, cn4.type, cn5.address, cn5.type, cn1.isApproved, cn2.isApproved, cn3.isApproved, cn4.isApproved, cn5.isApproved])
-
-  const getItem = ({ type, address, amount, tokenId, recipient }) => {
-    if (type === "NATIVE") {
-      return getItemETH(amount, amount, recipient)
-    } else if (type === "20") {
-      return getItem20(address, amount, amount, recipient)
-    } else if (type === "721") {
-      return getItem721(address, tokenId, 1, 1, recipient)
-    } else {
-      return getItem1155(address, tokenId, amount, amount, recipient)
-    }
-  }
+    cn1.token && checkIsApproved(cn1, setCn1)
+    cn2.token && checkIsApproved(cn2, setCn2)
+    cn3.token && checkIsApproved(cn3, setCn3)
+    cn4.token && checkIsApproved(cn4, setCn4)
+    cn5.token && checkIsApproved(cn5, setCn5)
+  }, [checkIsApproved, cn1.token, cn1.type, cn2.token, cn2.type, cn3.token, cn3.type, cn4.token, cn4.type, cn5.token, cn5.type, cn1.isApproved, cn2.isApproved, cn3.isApproved, cn4.isApproved, cn5.isApproved])
 
   const create = async () => {
     const offer = []
     const consideration = []
-    offer1.address && offer.push(getItem(offer1))
-    offer2.address && offer.push(getItem(offer2))
-    offer3.address && offer.push(getItem(offer3))
-    offer4.address && offer.push(getItem(offer4))
-    offer5.address && offer.push(getItem(offer5))
-    cn1.amount && consideration.push(getItem(cn1))
-    cn2.amount && consideration.push(getItem(cn2))
-    cn3.amount && consideration.push(getItem(cn3))
-    cn4.amount && consideration.push(getItem(cn4))
-    cn5.amount && consideration.push(getItem(cn5))
-    const { order, value } = await createOrder(
-      marketplaceContract,
-      chainId,
-      signer,
-      zone,
+    offer1.token && offer.push(sdk.getItem(offer1))
+    offer2.token && offer.push(sdk.getItem(offer2))
+    offer3.token && offer.push(sdk.getItem(offer3))
+    offer4.token && offer.push(sdk.getItem(offer4))
+    offer5.token && offer.push(sdk.getItem(offer5))
+    cn1.startAmount && consideration.push(sdk.getItem(cn1))
+    cn2.startAmount && consideration.push(sdk.getItem(cn2))
+    cn3.startAmount && consideration.push(sdk.getItem(cn3))
+    cn4.startAmount && consideration.push(sdk.getItem(cn4))
+    cn5.startAmount && consideration.push(sdk.getItem(cn5))
+    const { order, value, orderComponents, orderHash } = await sdk.createOrder(
       offer,
       consideration,
-      1, // PARTIAL_OPEN
+      1,
+      // zone,
       // [],
-      // null,
+      // constants.HashZero,
+      // constants.HashZero
+      // true // extraCheap
     )
     setOrder(order)
     setValue(value)
   }
-
   const fullfill = async () => {
-    order.numerator = numerator; // fill two tenths or one fifth
-    order.denominator = denominator; // fill two tenths or one fifth
-    const tx = marketplaceContract
-      .connect(signer)
-      .fulfillAdvancedOrder(
-        order,
-        [],
-        toKey(0),
-        constants.AddressZero,
-        {
-          value,
-        }
-      );
-    const receipt = await (await tx).wait();
-    console.log(receipt)
+    order.numerator = numerator
+    order.denominator = denominator
+    const tx = await sdk.fulfillOrder(order, value)
+    await tx.wait();
   }
 
   const approve = async (offer, setOffer) => {
-    const { type, address } = offer
-    let contract
-    let tx
-    if (type === "20") {
-      contract = new Contract(address, erc20ABI, eProvider)
-      tx = contract.connect(signer).approve(seaport, parseEther("10000000000"))
-    } else {
-      contract = new Contract(address, approveABI, eProvider)
-      tx = contract.connect(signer).setApprovalForAll(seaport, true)
-    }
-    await (await tx).wait();
+    const tx = await sdk.approveAsset(offer)
+    await tx.wait();
     setOffer({ ...offer, isApproved: true })
   }
 
   return (
     <div className={styles.container}>
-      {console.log(typeof numerator)}
       <main className={styles.main}>
         <W3WalletDriver />
         <div className="mb-5">
